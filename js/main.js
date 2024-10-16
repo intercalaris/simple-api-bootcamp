441a97773b93e6a15ec86e69f6d4472e801133d4
intercalaris <ymaviv@gmail.com>
2024-10-16 13:44:48 -0400

    complete

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import {apiKey} from "./apiKey.js";
 document.querySelector('button').addEventListener('click', getConversion)
-// https://v6.exchangerate-api.com/v6/
+// https://www.exchangerate-api.com/
 
 function getConversion(){
     const startCurr = document.querySelector('#start-curr').value;
