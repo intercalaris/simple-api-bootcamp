15edbdd38c43511e5a3cbe02109bd88416d219b1
intercalaris <ymaviv@gmail.com>
2024-10-16 00:58:41 -0400

    basic project completed

---
+++ js/main.js
@@ -0,0 +1,20 @@
+import {apiKey} from "./apiKey.js";
+document.querySelector('button').addEventListener('click', getConversion)
+// https://www.census.gov/data/developers/data-sets/census-microdata-api.html
+
+function getConversion(){
+    const startCurr = document.querySelector('#start-curr').value;
+    const targetCurr = document.querySelector('#target-curr').value;
+    const url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${startCurr}`;
+    
+    fetch(url)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+            // console.log(data)
+            console.log(data.conversion_rates[targetCurr])
+            document.querySelector('#conversion').innerText = `1 ${startCurr} = ${data.conversion_rates[targetCurr]} ${targetCurr}`
+        })
+        .catch(err => {
+        console.log(`error ${err}`)
+    });
+}
